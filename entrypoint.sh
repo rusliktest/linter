025a77f6029736e4f5ee4656498b4d27e250d5a3
rusliktest <rusliksemp@gmail.com>
2021-07-27 17:53:55 +0300

    release v4.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,32 @@
 #!/bin/bash -l
 
-echo "======================"
-echo "= Linting YAML files ="
-echo "======================"
+# echo "======================"
+# echo "= Linting YAML files ="
+# echo "======================"
 
-if [[ -n "$INPUT_CONFIG_FILE" ]]; then
-    options+=(-c "$INPUT_CONFIG_FILE")
-fi
+# if [[ -n "$INPUT_CONFIG_FILE" ]]; then
+#     options+=(-c "$INPUT_CONFIG_FILE")
+# fi
 
-if [[ -n "$INPUT_CONFIG_DATA" ]]; then
-    options+=(-d "$INPUT_CONFIG_DATA")
-fi
+# if [[ -n "$INPUT_CONFIG_DATA" ]]; then
+#     options+=(-d "$INPUT_CONFIG_DATA")
+# fi
 
-options+=(-f "$INPUT_FORMAT")
+# options+=(-f "$INPUT_FORMAT")
 
-if [[ "$INPUT_STRICT" == "true" ]]; then
-    options+=(-s)
-fi
+# if [[ "$INPUT_STRICT" == "true" ]]; then
+#     options+=(-s)
+# fi
 
-if [[ "$INPUT_NO_WARNINGS" == "true" ]]; then
-    options+=(--no-warnings)
-fi
+# if [[ "$INPUT_NO_WARNINGS" == "true" ]]; then
+#     options+=(--no-warnings)
+# fi
 
-# Enable globstar so ** globs recursively
-shopt -s globstar
-# Use the current directory by default
-options+=("${INPUT_FILE_OR_DIR:-.}")
-shopt -u globstar
+# # Enable globstar so ** globs recursively
+# shopt -s globstar
+# # Use the current directory by default
+# options+=("${INPUT_FILE_OR_DIR:-.}")
+# shopt -u globstar
 
 yamllint **/\"*.{yaml,yml,.yamllint}\" 
 # "${options[@]}"
