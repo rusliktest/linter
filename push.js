be60fa9f649be4f983f349a3bf1ca86677792c3f
YusufovEpam <82205487+YusufovEpam@users.noreply.github.com>
2021-07-28 11:50:20 +0300

    Commit for: v11

---
+++ push.js
@@ -0,0 +1,19 @@
+const { exec } = require("child_process");
+
+const VERSION = 'v11'
+const BRANCH = 'main'
+
+exec(
+	`git add . & git commit -m "Commit for: ${VERSION}" & git tag -a -m "My release: ${VERSION}" ${VERSION} & git push origin ${BRANCH}`,
+	(error, stdout, stderr) => {
+		if (error) {
+			console.log("error", error)
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`Successful commit: ${stdout}`);
+	})
--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VERSION="v10"
-
-git add . & 
-git commit -m "release ${VERSION}" & 
-git tag -a -m "My action release ${VERSION}" ${VERSION} & 
-git push origin main --follow-tags
